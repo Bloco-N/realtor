b0e6d237fe668993db48c44f5947547d5e8a461e
iueu <ilwelisaac@hotmail.com>
2023-02-17 18:13:27 -0300

    faet: change IRealtorRepository signature

--- src/repositories/IRealtorRepository.ts
+++ src/repositories/IRealtorRepository.ts
@@ -3,7 +3,7 @@ import { UpdateRealtorRequest } from "../dtos/requests/UpdateRealtorRequest";
 import { RealtorResponse } from "../dtos/responses/RealtorResponse";
 
 export interface IRealtorRepository {
-  findAll: () => Promise<RealtorResponse[]>
+  findAll: (search: string, page: number, offset: number) => Promise<RealtorResponse[]>
   get: (id: number) => Promise<RealtorResponse>
   add: (data: CreateRealtorRequest) => Promise<String>
   update: (data: UpdateRealtorRequest) => Promise<String>
