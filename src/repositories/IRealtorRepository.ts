96d092430e2868f9ea19768718c4f7db9390fc16
iueu <ilwelisaac@hotmail.com>
2023-02-18 20:40:45 -0300

    fix: changes to pass in tests

--- src/repositories/IRealtorRepository.ts
+++ src/repositories/IRealtorRepository.ts
@@ -4,7 +4,7 @@ import { RealtorResponse }      from '../dtos/responses/RealtorResponse'
 
 export interface IRealtorRepository {
   findAll: (search: string, page: number, offset: number) => Promise<RealtorResponse[]>
-  get: (id: number) => Promise<RealtorResponse>
+  get: (id: number) => Promise<RealtorResponse | Error>
   add: (data: CreateRealtorRequest) => Promise<string>
   update: (data: UpdateRealtorRequest) => Promise<string>
   remove: (id: number) => Promise<string>
