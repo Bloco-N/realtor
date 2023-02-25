9150a3c3b935d832fa18e04951b4eb2834323fa5
iueu <ilwelisaac@hotmail.com>
2023-02-25 20:26:35 -0300

    feat: error handling

--- src/controllers/RealtorController.ts
+++ src/controllers/RealtorController.ts
@@ -5,6 +5,7 @@ import { ApiError }             from '../errors/ApiError'
 import { RealtorRepository }    from '../repositories/RealtorRepository'
 import { Request, Response }    from 'express'
 import { validationResult }     from 'express-validator'
+import errorHandling            from '../handlers/errorHandling'
 
 export class RealtorController {
 
@@ -13,37 +14,47 @@ export class RealtorController {
   public async listAll(req: Request<unknown, unknown, unknown, ListAllRealtorsQuery>, res: Response) {
 
     try {
-      
-      const errors = validationResult(req).array()
-  
-      if(errors.length > 0){
 
+      const errors = validationResult(req).array()
+    
+      if(errors.length > 0){
+  
         const error = errors[0].msg as ApiError
         throw new ApiError(error.status, error.message)
-
-      }
   
+      }
+    
       const {
         query: { search, page, offset }
       } = req
-
+  
       const realtors = await this.repository.findAll(search, Number(page), Number(offset))
       res.status(200).send(realtors)
+      
+    } catch (error) {
+
+      errorHandling(res, error)
+    
+    }
+
+  }
+
+  public async signIn(req:Request, res:Response){
+
+    try {
+      
+      const { body } = req
+        
+      const token = await this.repository.signIn(body)
+        
+      res.status(200).send(token)
     
     } catch (error) {
 
-      if (error instanceof ApiError) {
-
-        res.status(error.status).send(error.message)
-      
-      }else{
-
-        res.status(500).send('internal errror')
-
-      }
+      errorHandling(res, error)
     
     }
-  
+
   }
 
   public async get(req: Request, res: Response) {
@@ -52,20 +63,12 @@ export class RealtorController {
 
       const { id } = req.params
       const realtor = await this.repository.get(Number(id))
-
+  
       res.status(200).send(realtor)
-    
+      
     } catch (error) {
 
-      if (error instanceof ApiError) {
-
-        res.status(error.status).send(error.message)
-      
-      }else{
-
-        res.status(500).send('internal errror')
-
-      }
+      errorHandling(res, error)
     
     }
   
@@ -73,17 +76,33 @@ export class RealtorController {
 
   public async add(req: Request<unknown, unknown, CreateRealtorRequest>, res: Response) {
 
-    const { body } = req
-    const created = await this.repository.add(body)
-    res.status(201).send(created)
+    try {
+
+      const { body } = req
+      const created = await this.repository.add(body)
+      res.status(201).send(created)
+      
+    }catch (error) {
+
+      errorHandling(res, error)
+    
+    }
   
   }
 
   public async update(req: Request<unknown, unknown, UpdateRealtorRequest>, res: Response) {
 
-    const { body } = req
-    const updated = await this.repository.update(body)
-    res.status(200).send(updated)
+    try {
+
+      const { body } = req
+      const updated = await this.repository.update(body)
+      res.status(200).send(updated)
+      
+    } catch (error) {
+
+      errorHandling(res, error)
+    
+    }
   
   }
 
@@ -93,20 +112,12 @@ export class RealtorController {
 
       const { id } = req.params
       const realtor = await this.repository.remove(Number(id))
-
+  
       res.status(200).send(realtor)
-    
-    } catch (error) {
-
-      if (error instanceof ApiError) {
-
-        res.status(error.status).send(error.message)
       
-      }else{
+    }catch (error) {
 
-        res.status(500).send('internal errror')
-
-      }
+      errorHandling(res, error)
     
     }
   
