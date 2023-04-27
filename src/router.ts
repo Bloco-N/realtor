46d686a757679f90039e8938f54bfdee6679926e
Aldebaran <elton.alde@gmail.com>
2023-04-27 17:00:19 -0300

    feat: agency routes

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,5 @@
+import { AgencyController }    from './controllers/AgencyController'
+import { ClientController }    from './controllers/ClientController'
 import { RealtorController }   from './controllers/RealtorController'
 import { ServiceController }   from './controllers/ServiceController'
 import { Auth }                from './middlewares/Auth'
@@ -8,6 +10,8 @@ import { checkSchema }         from 'express-validator'
 const router = Router()
 const realtorController = new RealtorController()
 const serviceController = new ServiceController()
+const agencyController = new AgencyController()
+const clientController = new ClientController()
 const auth = new Auth()
 
 router.get('/realtor', checkSchema(ListAllValidationSchema), realtorController.listAll.bind(realtorController))
@@ -16,6 +20,20 @@ router.post('/realtor/sign-in', realtorController.signIn.bind(realtorController)
 router.post('/realtor/sign-up', realtorController.add.bind(realtorController))
 router.put('/realtor', auth.realtorAuth, realtorController.update.bind(realtorController))
 router.delete('/realtor/:id', realtorController.remove.bind(realtorController))
+
+router.get('/agency', agencyController.listAll.bind(agencyController))
+router.get('/agency/:id', agencyController.get.bind(agencyController))
+router.post('/agency/sign-in', agencyController.signIn.bind(agencyController))
+router.post('/agency/sign-up', agencyController.add.bind(agencyController))
+router.put('/agency', agencyController.update.bind(agencyController))
+router.delete('/agency/:id', agencyController.remove.bind(agencyController))
+
+router.get('/client', clientController.listAll.bind(clientController))
+router.get('/client/:id', clientController.get.bind(clientController))
+router.post('/client/sign-in', clientController.signIn.bind(clientController))
+router.post('/client/sign-up', clientController.add.bind(clientController))
+router.put('/client', clientController.update.bind(clientController))
+router.delete('/client/:id', clientController.remove.bind(clientController))
 
 router.get('/property/realtor/:realtorId', realtorController.listAllProperties.bind(realtorController))
 router.post('/property', realtorController.addProperty.bind(realtorController))
