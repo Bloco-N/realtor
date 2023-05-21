ffeebdb99f35798bfc7fdf00e7e0db2934b12c82
iueu <ilwelisaac@hotmail.com>
2023-05-21 11:41:05 -0300

    feat: remove user mock

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -1,88 +1,6 @@
 import { PrismaClient } from '@prisma/client'
-import { hash }         from 'bcryptjs'
 
 const prisma = new PrismaClient()
-
-const mockRealtors = [
-  {
-    email: 'realtor1@mail.com',
-    firstName: 'realtor',
-    lastName: '1',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor2@mail.com',
-    firstName: 'realtor',
-    lastName: '2',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor3@mail.com',
-    firstName: 'realtor',
-    lastName: '3',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor4@mail.com',
-    firstName: 'realtor',
-    lastName: '4',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor5@mail.com',
-    firstName: 'realtor',
-    lastName: '5',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor6@mail.com',
-    firstName: 'realtor',
-    lastName: '6',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor7@mail.com',
-    firstName: 'realtor',
-    lastName: '7',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor8@mail.com',
-    firstName: 'realtor',
-    lastName: '8',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor9@mail.com',
-    firstName: 'realtor',
-    lastName: '9',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor10@mail.com',
-    firstName: 'realtor',
-    lastName: '10',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor11@mail.com',
-    firstName: 'realtor',
-    lastName: '11',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor12@mail.com',
-    firstName: 'realtor',
-    lastName: '12',
-    password: 'realtorPassword'
-  },
-  {
-    email: 'realtor13@mail.com',
-    firstName: 'realtor',
-    lastName: '13',
-    password: 'realtorPassword'
-  }
-]
 
 const mockServices = [
   'Acompanhamento para compra de imóvel',
@@ -93,21 +11,6 @@ const mockServices = [
 ]
 
 async function main() {
-
-  for (const item of mockRealtors) {
-
-    const hashPassowrd = await hash(item.password, 10)
-
-    await prisma.realtor.create({
-      data: {
-        email: item.email,
-        firstName: item.firstName,
-        lastName: item.lastName,
-        password: hashPassowrd
-      }
-    })
-  
-  }
 
   for (const item of mockServices) {
 
