f5e4a48af94263624f0e55e1ec86c152439ef8af
iueu <ilwelisaac@hotmail.com>
2023-06-08 16:18:25 -0300

    fix: fixing path

--- src/services/MailService.ts
+++ src/services/MailService.ts
@@ -7,7 +7,7 @@ export class MailService{
 
   public sendMail(to:string, subject:string, name:string, token:string){
 
-    const emailTemplate = fs.readFileSync(path.join(__dirname, "../templates/recover.hbs"), "utf-8")
+    const emailTemplate = fs.readFileSync(path.join(__dirname, "../../templates/recover.hbs"), "utf-8")
     const template = handlebars.compile(emailTemplate)
     const msgBody = (template({
       name,
