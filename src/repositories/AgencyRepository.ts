783fa587e391713c3bb57c4c8eed21e80eba7de0
iueu <ilwelisaac@hotmail.com>
2023-07-24 01:08:16 -0300

    fix: fixing agencyServices field name

--- src/repositories/AgencyRepository.ts
+++ src/repositories/AgencyRepository.ts
@@ -88,7 +88,7 @@ export class AgencyRepository {
             Language: true
           }
         },
-        AgencieServices:{
+        AgencyServices:{
           include:{
             Service: true
           }
